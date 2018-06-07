9b3ddfd3156e7ac40fefba1bac82ad6973780233
Srinidhi G S <srini.satti+lwt@gmail.com>
2018-06-07 23:22:29 +0530

    UI for given and formatted url

--- app/controllers/short_urls_controller.rb
+++ app/controllers/short_urls_controller.rb
@@ -1,5 +1,6 @@
 class ShortUrlsController < ApplicationController
   include ShortUrlsHelper
+  before_action :find_url, only: [:show, :destroy]
 
   def index
     @urls = ShortUrl.all
@@ -9,19 +10,25 @@ class ShortUrlsController < ApplicationController
     @url  = ShortUrl.new
   end
 
-  def show
-    @url = ShortUrl.find(params[:id])
-  end
-
   def create
     formatted_url = remove_url_prefix(params)
     url = ShortUrl.where(formatted_url: formatted_url).first_or_create(url_params)
     redirect_to short_url_path(url.id)
   end
 
+  def show
+  end
+
+  def destroy
+  end
+
   private
 
   def url_params
     params.require(:short_url).permit(:given_url)
+  end
+
+  def find_url
+    @url = ShortUrl.find(params[:id])
   end
 end
