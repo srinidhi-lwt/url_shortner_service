4ba9d3e36855dbde7a16cf76d2a4fdac956648db
Srinidhi G S <srini.satti+lwt@gmail.com>
2018-06-08 01:40:39 +0530

    use simple_form post request

--- app/controllers/url_views_controller.rb
+++ app/controllers/url_views_controller.rb
@@ -2,7 +2,7 @@ class UrlViewsController < ApplicationController
   include ApplicationHelper
 
 	def create
-		url = Url.find(params[:id])
+    url = ShortUrl.find(params[:short_url_id])
     url.url_views.create(url_views_params)
     redirect_to url.given_url
 	end
