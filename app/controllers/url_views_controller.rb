b819e49556988776254de5de6e525a1948384da4
Srinidhi G S <srini.satti+lwt@gmail.com>
2018-06-08 00:49:21 +0530

    create url views

---
+++ app/controllers/url_views_controller.rb
@@ -0,0 +1,19 @@
+class UrlViewsController < ApplicationController
+  include ApplicationHelper
+
+	def create
+		url = Url.find(params[:id])
+    url.url_views.create(url_views_params)
+    redirect_to url.given_url
+	end
+
+	private
+
+	def url_views_params
+    {}.tap do |x|
+      x[:ip_address] = client_ip
+      x[:browser_info] =  browser
+      x[:browser_platform] = browser.platform
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root 'short_urls#index'
 
   resources :short_urls
+  resources :url_views, only: [:create]
 end
