0a4a6e09ad50e29d1b926f4a0cd23370ada4e6d5
Srinidhi G S <srini.satti+lwt@gmail.com>
2018-06-08 15:59:13 +0530

    URL views dependent destroy and UI

--- app/models/short_url.rb
+++ app/models/short_url.rb
@@ -1,8 +1,9 @@
 class ShortUrl < ApplicationRecord
   SHORT_URL_LENGTH = 7
 
-  has_many :url_views
+  has_many :url_views, dependent: :destroy
   validates :given_url, :formatted_url, presence: true
+  validates_format_of :given_url, with: /\A(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w\.-]*)*\/?\Z/i
 
   before_create :shorten_url
 
